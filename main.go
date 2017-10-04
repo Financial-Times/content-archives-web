8032f75764a174e9d96fae7e71fa08ed8bdfd5a7
Ciont, Ioana <Ioana.Ciont@ft.com>
2017-10-04 10:53:19 +0300

    Adding download functionality

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 	"os"
 
-	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
-
-func exitErrorf(msg string, args ...interface{}) {
-	fmt.Fprintf(os.Stderr, msg+"\n", args...)
-	os.Exit(1)
-}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -32,68 +20,42 @@ func main() {
 		log.Fatal("$AWS_REGION must be set")
 	}
 
+	awsBucketName := os.Getenv("AWS_BUCKET_NAME")
+	if awsBucketName == "" {
+		log.Fatal("$AWS_BUCKET_NAME must be set")
+	}
+
+	awsBucketPrefix := os.Getenv("AWS_BUCKET_PREFIX")
+	if awsBucketPrefix == "" {
+		log.Fatal("$AWS_BUCKET_PREFIX must be set")
+	}
+
+	s3Reader := NewS3Reader(awsRegion, awsBucketName, awsBucketPrefix)
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
-	router.SetFuncMap(template.FuncMap{
-		"downloadArchiveFromS3": downloadArchiveFromS3,
-	})
 
 	router.GET("/", func(c *gin.Context) {
+		zipFiles, err := s3Reader.RetrieveArchivesFromS3()
+		if err != nil {
+			c.HTML(http.StatusInternalServerError, "Unable to get archives list from S3", nil)
+		}
 		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
-			"zipFiles": retrieveArchivesFromS3(awsRegion),
+			"zipFiles": zipFiles,
 		})
+	})
+
+	router.GET("/download/yearly-archives/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		bytes, err := s3Reader.DownloadArchiveFromS3(name)
+		if err != nil {
+			c.HTML(http.StatusInternalServerError, "Unable to download archive from S3", nil)
+		}
+
+		c.Header("Content-Disposition", "attachment; filename="+name)
+		c.Data(http.StatusOK, "application/zip", bytes)
 	})
 
 	router.Run(":" + port)
-}
-
-func retrieveArchivesFromS3(awsRegion string) []string {
-	sess, err := session.NewSession()
-
-	// Create S3 service client
-	svc := s3.New(sess)
-	input := &s3.ListObjectsInput{
-		Bucket: aws.String(os.Getenv("BUCKET_NAME")),
-		Prefix: aws.String("yearly-archives"),
-	}
-
-	result, err := svc.ListObjects(input)
-	if err != nil {
-		log.Printf("Unable to list buckets, %v", err)
-	}
-	log.Println(result)
-
-	zipFiles := make([]string, 0)
-	for _, content := range result.Contents {
-		zipFiles = append(zipFiles, aws.StringValue(content.Key))
-	}
-
-	return zipFiles
-}
-
-func downloadArchiveFromS3(fileName string) {
-	sess, err := session.NewSession()
-	downloader := s3manager.NewDownloader(sess)
-
-	file, err := os.Create(fileName)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", err)
-	}
-	defer file.Close()
-
-	numBytes, err := downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(os.Getenv("BUCKET_NAME")),
-			Key:    aws.String(fileName),
-		})
-
-	if err != nil {
-		exitErrorf("Unable to download item %q, %v", fileName, err)
-	}
-
-	//_, err = io.Copy(fileName, resp.Body)
-
-	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
 }
---
+++ s3_reader.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+// S3Reader struct containig AWS configuration
+type S3Reader struct {
+	awsRegion       string
+	awsBucketName   string
+	awsBucketPrefix string
+}
+
+// NewS3Reader returns a new S3Reader instance
+func NewS3Reader(awsRegion string, awsBucketName string, awsBucketPrefix string) S3Reader {
+	return S3Reader{awsRegion, awsBucketName, awsBucketPrefix}
+}
+
+// RetrieveArchivesFromS3 returns a list of objects from S3, based on awsBucketName and awsBucketPrefix
+func (s3Reader *S3Reader) RetrieveArchivesFromS3() ([]string, error) {
+	zipFiles := make([]string, 0)
+	sess, err := session.NewSession()
+	if err != nil {
+		return zipFiles, err
+	}
+
+	svc := s3.New(sess)
+	input := &s3.ListObjectsInput{
+		Bucket: aws.String(s3Reader.awsBucketName),
+		Prefix: aws.String(s3Reader.awsBucketPrefix),
+	}
+
+	result, err := svc.ListObjects(input)
+	if err != nil {
+		return zipFiles, err
+	}
+
+	for _, content := range result.Contents {
+		zipFiles = append(zipFiles, aws.StringValue(content.Key))
+	}
+
+	return zipFiles, nil
+}
+
+// DownloadArchiveFromS3 downloads a file from AWS S3 and returns an array of bytes
+func (s3Reader *S3Reader) DownloadArchiveFromS3(fileName string) ([]byte, error) {
+	log.Println("Starting to download archives...")
+
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, err
+	}
+
+	downloader := s3manager.NewDownloader(sess)
+	var a []byte
+	buffer := aws.NewWriteAtBuffer(a)
+	_, err = downloader.Download(buffer,
+		&s3.GetObjectInput{
+			Bucket: aws.String(s3Reader.awsBucketName),
+			Key:    aws.String(s3Reader.awsBucketPrefix + "/" + fileName),
+		})
+
+	if err != nil {
+		log.Fatalf("Unable to download item %q, %v", fileName, err)
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
+}
