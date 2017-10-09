62d2d3581c207ae81f49d5ff6c8aaa460a06156a
Ciont, Ioana <Ioana.Ciont@ft.com>
2017-10-09 15:22:04 +0300

    Adding file size details & some text changes

--- s3_reader.go
+++ s3_reader.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"code.cloudfoundry.org/bytefmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -16,15 +17,21 @@ type S3Reader struct {
 	awsBucketPrefix string
 }
 
+// ZipFile object that holds details about an archive
+type ZipFile struct {
+	Name         string
+	Size         string
+	LastModified string
+}
+
 // NewS3Reader returns a new S3Reader instance
 func NewS3Reader(awsRegion string, awsBucketName string, awsBucketPrefix string) S3Reader {
 	return S3Reader{awsRegion, awsBucketName, awsBucketPrefix}
 }
 
 // RetrieveArchivesFromS3 returns a list of objects from S3, based on awsBucketName and awsBucketPrefix
-func (s3Reader *S3Reader) RetrieveArchivesFromS3() (map[string]string, error) {
-	//zipFiles := make([]string, 0)
-	zipFiles := make(map[string]string)
+func (s3Reader *S3Reader) RetrieveArchivesFromS3() ([]ZipFile, error) {
+	zipFiles := make([]ZipFile, 0)
 	sess, err := session.NewSession()
 	if err != nil {
 		return zipFiles, err
@@ -35,7 +42,6 @@ func (s3Reader *S3Reader) RetrieveArchivesFromS3() (map[string]string, error) {
 		Bucket: aws.String(s3Reader.awsBucketName),
 		Prefix: aws.String(s3Reader.awsBucketPrefix),
 	}
-
 	result, err := svc.ListObjects(input)
 	if err != nil {
 		return zipFiles, err
@@ -43,7 +49,12 @@ func (s3Reader *S3Reader) RetrieveArchivesFromS3() (map[string]string, error) {
 
 	for _, content := range result.Contents {
 		lastModified := aws.TimeValue(content.LastModified)
-		zipFiles[aws.StringValue(content.Key)] = lastModified.Format("2006-01-02 15:04:05")
+		lastModifiedStr := lastModified.Format("2006-01-02 15:04:05")
+		size := aws.Int64Value(content.Size)
+		sizeMB := bytefmt.ByteSize(uint64(size))
+
+		file := ZipFile{aws.StringValue(content.Key), sizeMB, lastModifiedStr}
+		zipFiles = append(zipFiles, file)
 	}
 
 	return zipFiles, nil
